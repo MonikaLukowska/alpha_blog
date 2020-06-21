65e21ce97479e97f1ab209b0b80d14badefac986
Sebastian <monika.lukowska@gmail.com>
2020-06-21 22:29:00 +0200

    add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@
 
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update distroy]
+
+  def show
+  @articles = @user.articles
+  end
+
   def new
     @user = User.new
   end
