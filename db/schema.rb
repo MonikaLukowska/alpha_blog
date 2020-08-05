fcfb5f878515fbf38c85380476c3ebcd66c3a6d7
Sebastian <monika.lukowska@gmail.com>
2020-08-05 19:48:25 +0200

    add many to many association, create article_category model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_192533) do
+ActiveRecord::Schema.define(version: 2020_07_26_192002) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
