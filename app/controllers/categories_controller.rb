5710250974459f6a321689fdcbf8cf2a50fc3a6c
Sebastian <monika.lukowska@gmail.com>
2020-08-05 20:50:15 +0200

    add edit action and view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -26,6 +26,20 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if@category.update(category_params)
+      flash[:notice] = 'Category name updated successfuly'
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def category_params
